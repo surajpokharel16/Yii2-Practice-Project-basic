e0c0b285066f6d8b5dc0892664f051cd4fecc446
surajpokhrel16 <surajpokhrel16@gmail.com>
2022-02-04 20:13:18 +0000

    Adding migration

---
+++ migrations/m220204_201126_create_table_place.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220204_201126_create_table_place
+ */
+class m220204_201126_create_table_place extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220204_201126_create_table_place cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220204_201126_create_table_place cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
